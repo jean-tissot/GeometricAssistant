bd845ae71febea0903005cda4161a8040987af8e
jean-tissot <asphjt@gmail.com>
2022-04-11 23:37:50 +0200

    Renames home to polygon

--- app/src/main/java/com/example/geometricassistant/ui/polygon/PolygonFragment.kt
+++ app/src/main/java/com/example/geometricassistant/ui/polygon/PolygonFragment.kt
@@ -1,4 +1,4 @@
-package com.example.geometricassistant.ui.home
+package com.example.geometricassistant.ui.polygon
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,13 +8,12 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.geometricassistant.R
-import com.example.geometricassistant.databinding.FragmentHomeBinding
+import com.example.geometricassistant.databinding.FragmentPolygonBinding
 
-class HomeFragment : Fragment() {
+class PolygonFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
-    private var _binding: FragmentHomeBinding? = null
+    private lateinit var polygonViewModel: PolygonViewModel
+    private var _binding: FragmentPolygonBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,14 +24,14 @@ class HomeFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
+        polygonViewModel =
+                ViewModelProvider(this).get(PolygonViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentPolygonBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+        val textView: TextView = binding.textPolygon
+        polygonViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
--- app/src/main/java/com/example/geometricassistant/ui/polygon/PolygonViewModel.kt
+++ app/src/main/java/com/example/geometricassistant/ui/polygon/PolygonViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.geometricassistant.ui.home
+package com.example.geometricassistant.ui.polygon
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class PolygonViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is polygon Fragment"
     }
     val text: LiveData<String> = _text
 }
